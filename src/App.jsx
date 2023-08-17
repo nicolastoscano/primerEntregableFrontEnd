e2f6fa3c72731b37b1a9dd2c4e4d337f0d037ab9
nicolastoscano <nicolastoscano98@gmail.com>
2023-08-17 18:45:14 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './Card'
 
 function App() {
 
   return (
-      <div className='App'>
-        <Card/>
-      </div>
+    <div className='App'>
+      <Card/>
+    </div>
   )
 }
 
